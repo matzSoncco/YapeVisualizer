9fb2375a719f5f7986692793b64d65ab313e26f0
Max Junior Soncco Mamani <msonccoma@unsa.edu.pe>
2026-01-20 23:58:43 -0500

    refactor: añade función de cierre de sesión

--- src/composables/useAuth.js
+++ src/composables/useAuth.js
@@ -1,30 +1,64 @@
-import { ref } from 'vue';
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { onMounted, ref } from 'vue';
+import { GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOut } from "firebase/auth";
 import { auth } from "../firebaseConfig";
+
+const user = ref (null);
+
+/**
+ * Funcionalidad de autenticación con Google
+ * @returns {Object} Métodos y propiedades de autenticación
+ */
 
 export function useAuth() {
     const error = ref(null);
     const loading = ref(false);
 
+    /**
+     * Monitorea el estado de autenticación del usuario
+     */
+    onMounted (() => {
+        onAuthStateChanged(auth, (currentUser) => {
+            user.value = currentUser;
+        })
+    })
+
+    /**
+     * Inicia sesión con Google
+     * @return {Promise<void>}
+     */
     const logInWithGoogle = async () => {
         error.value = null;
         loading.value = true;
         try {
             const provider = new GoogleAuthProvider();
-            const result = await signInWithPopup(auth, provider);
-            loading.value = false;
-
-            return result.user;
+            await signInWithPopup(auth, provider);
         } catch (err) {
             console.error("Error en auntenticación con Google:", err);
             error.value = "Error de conexión con Google. Por favor, inténtalo de nuevo.";
+        } finally {
             loading.value = false;
-            throw err;
         }
     }
 
+    /**
+     * Cierra la sesión del usuario
+     * @return {Promise<void>}
+     */
+    const logOut = async () => {
+        try {
+            await signOut(auth);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    /**
+     * Retorna las propiedades y métodos del composable
+     */
     return {
+        user,
         logInWithGoogle,
+        logOut,
         error,
         loading
     }
