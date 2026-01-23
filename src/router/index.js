d361faf58f22897600082f7229adc24ec38dedcc
Max Junior Soncco Mamani <msonccoma@unsa.edu.pe>
2026-01-23 10:59:04 -0500

    feat: añade la ruta de admin para el panel de administracion del usuario

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
 import DashboardView from '../views/DashboardView.vue'
+import AdminView from '@/views/AdminView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,8 +15,15 @@ const router = createRouter({
       path: '/dashboard',
       name: 'dashboard',
       component: DashboardView
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: AdminView
     }
   ]
 })
+
+//TODO: Agregar logica de beforeEach para proteger rutas segun auth y sucursal seleccionada
 
 export default router
