2452a1b869fe667139600e7874b14e98d6a0e825
Max Junior Soncco Mamani <msonccoma@unsa.edu.pe>
2026-01-28 00:09:53 -0500

    feat: uso de servicios de primevue para su utilizacion en el proyecto

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import ToastService from 'primevue/toastservice'
+import ConfirmationService from 'primevue/confirmationservice'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(ToastService)
+app.use(ConfirmationService)
 
 app.mount('#app')
