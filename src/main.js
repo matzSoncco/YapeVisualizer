813b4a9ad45ec2ff665274c0403883b5ee1b5d08
Max Junior Soncco Mamani <msonccoma@unsa.edu.pe>
2026-02-14 01:54:55 -0500

    doc: actualizando

--- src/main.js
+++ src/main.js
@@ -10,6 +10,9 @@
  * @autor matzSoncco
  * @coautor pepo (hizo el apk movil y parte del backend xd)
  * @año 2026
+ * @mes febrero
+ * @hora 1:54 AM
+ * @version 1.0.0
  */
 
 import { createApp } from 'vue'
